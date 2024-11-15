6d7e4ae77cbcdbdde3ece6451534d1f89a2c0971
Vachek-hooves <vachek0602@gmail.com>
2024-11-15 17:39:00 +0200

    create blank components

---
+++ components/screenComponents/MainScreen/PostCardDetails.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const PostCardDetails = () => {
+  return (
+    <View>
+      <Text>PostCardDetails</Text>
+    </View>
+  )
+}
+
+export default PostCardDetails
+
+const styles = StyleSheet.create({})
---
+++ components/screenComponents/MainScreen/index.js
@@ -0,0 +1,7 @@
+import DailyQuiz from "./DailyQuiz";
+import PostCard from "./PostCard";
+import PostDetails from "./PostCardDetails";
+import DailyFacts from "./DailyFacts";
+import DaylyFactDetails from "./DaylyFactDetails";
+import FactCard from "./FactCard";
+export { DailyQuiz, PostCard, PostDetails, DailyFacts, DaylyFactDetails, FactCard };    
