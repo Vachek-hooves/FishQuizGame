fae0a785dde2bc81eefd284a8bcf8e9b8cc67440
Vachek-hooves <vachek0602@gmail.com>
2024-11-21 13:17:01 +0200

    rename the app

--- android/app/src/main/java/com/fishquizgame/MainActivity.kt
+++ android/app/src/main/java/com/fishquizgame/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "FishQuizGame"
+  override fun getMainComponentName(): String = "Fish Fact & Quiz Collection"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
